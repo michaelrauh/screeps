f66f52ceed5f16064150675e6a7dc63784d13174
michaelrauh <michaelrauh@ymail.com>
2018-11-18 09:01:39 -0500

    add check to see if the creep is at the correct location already

--- role.harvester.js
+++ role.harvester.js
@@ -13,12 +13,17 @@
 //         }
 //     }
 // };
+var _ = require('lodash');
 
 var roleHarvester = {
   run: function(creep, spawn) {
     var first_source_pos = creep.room.find(FIND_SOURCES)[0].pos;
-
-    creep.moveTo(first_source_pos.x -1, first_source_pos.y)
+    var desired_location = {x: first_source_pos.x -1, y: first_source_pos.y}
+    var current_location = {x: creep.pos.x, y: creep.pos.y}
+    
+    if (!_.isEqual(desired_location, current_location)) {
+      creep.moveTo(first_source_pos.x -1, first_source_pos.y)
+    }
   }
 };
 
--- role.harvester.test.js
+++ role.harvester.test.js
@@ -1,22 +1,31 @@
 const subject = require('./role.harvester')
 var td = require('testdouble')
 
-test('moves toward the first source found', () => {
+var creep
+
+beforeEach(() => {
   global.FIND_SOURCES = 105
   global.ERR_NOT_IN_RANGE = -9
   global.RESOURCE_ENERGY = 'energy'
 
-  expect(true).toEqual(true)
+  creep = td.object()
 
   var find = td.function("find")
   var room = td.object()
-  var creep = td.object()
   var moveTo = td.function("moveTo")
 
   room.find = find
   creep.room = room
   creep.moveTo = moveTo
   td.when(creep.room.find(FIND_SOURCES)).thenReturn([{pos: { x: 5, y: 17 }}])
+});
+
+afterEach(() => {
+  td.reset()
+});
+
+test('moves toward the first source found if it is not adjacent', () => {
+  creep.pos = {x: 1, y: 1}
 
   subject.run(creep, null)
 
