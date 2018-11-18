3eef59872691513b8659e071c661f810d4bbac7f
michaelrauh <michaelrauh@ymail.com>
2018-11-18 10:36:01 -0500

    refactor to use strategy pattern and beautify

--- role.harvester.js
+++ role.harvester.js
@@ -1,46 +1,39 @@
-// var roleHarvester = {
-//     run: function(creep, spawn) {
-//         if(creep.carry.energy < creep.carryCapacity) {
-//             var sources = creep.room.find(FIND_SOURCES);
-//             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-//                 creep.moveTo(sources[0]);
-//             }
-//         }
-//         else {
-//             if(creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-//                 creep.moveTo(spawn);
-//             }
-//         }
-//     }
-// };
 var _ = require('lodash');
 
 var roleHarvester = {
   run: function(creep, spawn) {
     var first_source = creep.room.find(FIND_SOURCES)[0];
-    var current_location = {x: creep.pos.x, y: creep.pos.y}
+    var current_location = {
+      x: creep.pos.x,
+      y: creep.pos.y
+    }
 
-    if(creep.carry.energy < creep.carryCapacity) {
+    if (creep.carry.energy < creep.carryCapacity) {
       var desired_location = findDesiredLocation(first_source)
+      var action = function() {
+        creep.harvest(first_source)
+      }
     } else {
       var desired_location = findDesiredLocation(spawn)
+      var action = function() {
+        creep.transfer(spawn, RESOURCE_ENERGY)
+      }
     }
 
     if (!_.isEqual(desired_location, current_location)) {
       creep.moveTo(desired_location.x, desired_location.y)
     } else {
-      if(creep.carry.energy < creep.carryCapacity) {
-        creep.harvest(first_source)
-      } else {
-        creep.transfer(spawn, RESOURCE_ENERGY)
-      }
+      action()
     }
   }
 };
 
 function findDesiredLocation(source) {
   var first_source_pos = source.pos
-  return {x: first_source_pos.x -1, y: first_source_pos.y}
+  return {
+    x: first_source_pos.x - 1,
+    y: first_source_pos.y
+  }
 }
 
 module.exports = roleHarvester;
