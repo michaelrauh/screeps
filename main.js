f7d7a1ab00609603028dc7a989f358e4bf6649e7
michaelrauh <michaelrauh@ymail.com>
2018-11-18 15:24:05 -0500

    add role to creep memory

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var roleHarvester = require('role.harvester');
 module.exports.loop = function () {
     var spawn = Game.spawns['Spawn1']
     if (Object.keys(Game.creeps).length < 4) {
-        spawn.spawnCreep([WORK, CARRY, MOVE], 'harvester' + Game.time);    
+        spawn.spawnCreep([WORK, CARRY, MOVE], 'harvester' + Game.time, {memory: {role: "harvester"}});    
     }
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
