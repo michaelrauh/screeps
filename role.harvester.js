8d0bb33278bf20e924c586a47ce108c00f0178ac
michaelrauh <michaelrauh@ymail.com>
2018-11-18 16:53:45 -0500

    break out mode selection in harvester to separate function

--- role.harvester.js
+++ role.harvester.js
@@ -1,35 +1,42 @@
 var roleHarvester = {
   run: function(creep, spawn) {
-    var action
-    var target
 
-    if (creep.carry.energy < creep.carryCapacity) {
-      var first_source = creep.room.find(FIND_SOURCES)[0];
-      target = first_source
-      action = function() {
-        creep.harvest(first_source)
-      }
-    } else {
-      target = spawn
-      action = function() {
-        creep.transfer(spawn, RESOURCE_ENERGY)
-      }
-    }
+    var mode = setMode(creep, spawn)
 
-    var desired_location = findDesiredLocation(target)
+    var desired_location = findDesiredLocation(mode.target)
 
     var current_location = {
       x: creep.pos.x,
       y: creep.pos.y
     }
-    
+
     if (adjacent(desired_location, current_location)) {
-      action()
+      mode.action()
     } else {
       creep.moveTo(desired_location.x, desired_location.y)
     }
   }
 };
+
+function setMode(creep, spawn) {
+  if (creep.carry.energy < creep.carryCapacity) {
+    var first_source = creep.room.find(FIND_SOURCES)[0];
+    target = first_source
+    action = function() {
+      creep.harvest(first_source)
+    }
+  } else {
+    target = spawn
+    action = function() {
+      creep.transfer(spawn, RESOURCE_ENERGY)
+    }
+  }
+
+  return {
+    target: target,
+    action: action
+  }
+}
 
 function findDesiredLocation(location) {
   return {
