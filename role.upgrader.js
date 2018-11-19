042277815979b46f36e602cd22eba663e337f02c
michaelrauh <michaelrauh@ymail.com>
2018-11-19 17:58:25 -0500

    broke out setBias to a private function

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,17 +3,21 @@ var util = require("./util")
 
 var roleUpgrader = {
     run: function(creep, spawn) {
-      if (creep.carry.energy == creep.carryCapacity) {
-        creep.memory.bias = "upgrade"
-      }
-
-      if (creep.carry.energy == 0) {
-        creep.memory.bias = "harvest"
-      }
+      setBias(creep)
 
       util.approachAndAct(creep, setMode(creep, spawn))
     }
 };
+
+function setBias(creep) {
+  if (creep.carry.energy == creep.carryCapacity) {
+    creep.memory.bias = "upgrade"
+  }
+
+  if (creep.carry.energy == 0) {
+    creep.memory.bias = "harvest"
+  }
+}
 
 function setMode(creep, spawn) {
   if (creep.memory.bias == "harvest") {
