adb03653f2a1041eb7dbf2ab192de65e78c7cd5e
michaelrauh <michaelrauh@ymail.com>
2018-11-19 17:49:08 -0500

    introduce a bias in the upgrader to continue upgrading until out of energy

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,12 +3,20 @@ var util = require("./util")
 
 var roleUpgrader = {
     run: function(creep, spawn) {
+      if (creep.carry.energy == creep.carryCapacity) {
+        creep.memory.bias = "upgrade"
+      }
+
+      if (creep.carry.energy == 0) {
+        creep.memory.bias = "harvest"
+      }
+
       util.approachAndAct(creep, setMode(creep, spawn))
     }
 };
 
 function setMode(creep, spawn) {
-  if (creep.carry.energy < creep.carryCapacity) {
+  if (creep.memory.bias == "harvest") {
     return modes.harvest(creep.room.find(FIND_SOURCES)[0], creep)
   } else {
     return modes.upgradeRoom(creep, creep.room.controller)
--- role.upgrader.test.js
+++ role.upgrader.test.js
@@ -9,6 +9,8 @@ var creep
 var spawn
 var roomController
 var source
+var harvestMode
+var upgradeMode
 
 beforeEach(() => {
   global.RESOURCE_ENERGY = 'energy'
@@ -16,34 +18,53 @@ beforeEach(() => {
 
   spawn = {}
   roomController = {}
-  creep = {carry: {energy: null}, room: {controller: roomController, find: td.function("find")}}
+  creep = {carryCapacity: 12, memory: {bias: "harvest"}, carry: {energy: null}, room: {controller: roomController, find: td.function("find")}}
   source = {pos: { x: 5, y: 17 }}
+  harvestMode = {harvest: "mode"}
+  td.when(creep.room.find(FIND_SOURCES)).thenReturn([source])
+  td.when(modes.harvest(source, creep)).thenReturn(harvestMode)
+
+  upgradeMode = {upgrade: "mode"}
+  td.when(modes.upgradeRoom(creep, roomController)).thenReturn(upgradeMode)
 });
 
 afterEach(() => {
   td.reset()
 });
 
-test('approaches and acts with harvest mode when carrying less than max energy', () => {
-  var harvestMode = {harvest: "mode"}
-  creep.carry.energy = 2
-  creep.carryCapacity = 7
-
-  td.when(creep.room.find(FIND_SOURCES)).thenReturn([source])
-  td.when(modes.harvest(source, creep)).thenReturn(harvestMode)
-
+test('approaches and acts with harvest mode when bias is harvest', () => {
+  creep.carry.energy = 0
   subject.run(creep, spawn)
 
   td.verify(util.approachAndAct(creep, harvestMode))
 })
 
-test('approaches and acts with upgrade mode when carrying max energy', () => {
-  var upgradeMode = {upgrade: "mode"}
-  td.when(modes.upgradeRoom(creep, roomController)).thenReturn(upgradeMode)
+test('approaches and acts with upgrade mode when bias is upgrade', () => {
   creep.carry.energy = 12
-  creep.carryCapacity = 12
+  creep.memory.bias = "upgrade"
 
   subject.run(creep, spawn)
 
   td.verify(util.approachAndAct(creep, upgradeMode))
+})
+
+test('changes bias to upgrade when energy is full', () => {
+  creep.carry.energy = 12
+
+  creep.memory.bias = "harvest"
+
+  subject.run(creep, spawn)
+
+  expect(creep.memory.bias).toEqual("upgrade")
+})
+
+test('changes bias to harvest when energy is empty', () => {
+  creep.carry.energy = 0
+  creep.carryCapacity = 12
+
+  creep.memory.bias = "upgrade"
+
+  subject.run(creep, spawn)
+
+  expect(creep.memory.bias).toEqual("harvest")
 })
