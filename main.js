2007a94ef60669392939cfe81123a1b688fea125
michaelrauh <michaelrauh@ymail.com>
2018-11-19 12:51:37 -0500

    quick change to main to make upgraders as well as harvesters. This is making it clear that the upgraders are only upgrading one energy before going to harvest more

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
     var spawn = Game.spawns['Spawn1']
-    if (Object.keys(Game.creeps).length < 4) {
-        spawn.spawnCreep([WORK, CARRY, MOVE], 'harvester' + Game.time, {memory: {role: "harvester"}});    
+    var num_creeps = Object.keys(Game.creeps).length
+    if (num_creeps < 4) {
+        if (num_creeps % 2 == 0) {
+            spawn.spawnCreep([WORK, CARRY, MOVE], 'harvester' + Game.time, {memory: {role: "harvester"}});    
+        } else {
+            spawn.spawnCreep([WORK, CARRY, MOVE], 'upgrader' + Game.time, {memory: {role: "upgrader"}});    
+        }
     }
+            
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        roleHarvester.run(creep, spawn);
+        if (creep.memory.role == "harvester") {
+            roleHarvester.run(creep, spawn);    
+        } else {
+            roleUpgrader.run(creep, spawn);
+        } 
     }
 }
