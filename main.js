41151a727e703ecd9aa4dfb8aba9ba9065aa1f14
michaelrauh <michaelrauh@ymail.com>
2018-11-21 14:56:10 -0500

    add call to extensionManager in main loop. This introduces a performance issue as it calls a lot and rarely does anything

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var extensionManager = require('extensionManager')
 
 module.exports.loop = function () {
     var spawn = Game.spawns['Spawn1']
+
+    extensionManager.run(spawn, spawn.room)
     var num_creeps = Object.keys(Game.creeps).length
     if (num_creeps < 4) {
         if (num_creeps % 2 == 0) {
-            spawn.spawnCreep([WORK, CARRY, MOVE], 'harvester' + Game.time, {memory: {role: "harvester"}});    
+            spawn.spawnCreep([WORK, CARRY, MOVE], 'harvester' + Game.time, {memory: {role: "harvester"}});
         } else {
-            spawn.spawnCreep([WORK, CARRY, MOVE], 'upgrader' + Game.time, {memory: {role: "upgrader"}});    
+            spawn.spawnCreep([WORK, CARRY, MOVE], 'upgrader' + Game.time, {memory: {role: "upgrader"}});
         }
     }
-            
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if (creep.memory.role == "harvester") {
-            roleHarvester.run(creep, spawn);    
+            roleHarvester.run(creep, spawn);
         } else {
             roleUpgrader.run(creep, spawn);
-        } 
+        }
     }
 }
